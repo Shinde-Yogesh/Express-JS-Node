a8d4406b028d68fb1d90ff5a7b819c2ff5688d3f
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-21 13:10:09 +0530

    install middleware package and test using local server

---
+++ 3.4 Middleware/index1.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+//use an method
+// where as urlencoded is used to which type of data we got from the page
+// body parser is used to return the value
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// for handle the the erlencoded
+app.post("/submit",(req, res) =>
+{
+  console.log(req.body)
+})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
